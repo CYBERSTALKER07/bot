9399870630848c97290f0237a89793e9d5653da3
CYBERTSTALKER07 <shakhzodsoliyevgates@gmail.com>
2025-09-14 23:02:50 +0500

    refactor: Remove unused ESLint directive for cleaner code

--- src/components/MobileBottomNavigation.tsx
+++ src/components/MobileBottomNavigation.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+ 
 import React, { useState, useRef, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AddEntryModal } from './AddEntryModal';
